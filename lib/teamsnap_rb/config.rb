59bd1c3f29875026c65b4a1f12091008f4bed645
Ziemek Wolski <ziemek.wolski@gmail.com>
2015-01-15 22:05:39 -0500

    Revert "Added access via authorization code." --accidentally commited to master
    This reverts commit e44c26fe66be4d951195e4e219c263e93ffb747e.

--- lib/teamsnap_rb/config.rb
+++ lib/teamsnap_rb/config.rb
@@ -1,9 +1,8 @@
 module TeamsnapRb
   class Config
-    attr_accessor :access_token, :client_id, :client_secret, :request_middleware, :response_middleware, :authorization
+    attr_accessor :access_token, :client_id, :client_secret, :request_middleware, :response_middleware
 
     def initialize(options = {})
-      self.authorization = options.fetch(:authorization, nil)
       self.access_token = options.fetch(:access_token, nil)
       self.client_secret = options.fetch(:client_secret, nil)
       self.client_id = options.fetch(:client_id, nil)
